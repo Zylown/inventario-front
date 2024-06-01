7b544831bb50ad7930e131711c13a617aed2495e
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-05-31 23:03:55 -0500

    add new tailwind colors + responsive layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       colors: {
         glass: "rgba(255, 255, 255, 0.25)",
         brown: "rgb(30,30,17)",
+        carbon: "rgb(34,34,34)",
+        "verde-claro": "rgb(4,87,87)",
+        "verde-oscuro": "rgb(4,67,67)",
+        crema: "rgb(228,228,228)",
       },
     },
   },
