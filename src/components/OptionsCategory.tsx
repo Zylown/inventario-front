40cb8a6c71ebec43ad7e4e632f0f433e5db4b382
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-06-08 22:28:17 -0500

    add validations for FormAdd + add OptionsCategory

---
+++ src/components/OptionsCategory.tsx
@@ -0,0 +1,8 @@
+export default function OptionsCategory() {
+  return (
+    <select>
+      <option value="1">BOLSA</option>
+      <option value="2">TODOS</option>
+    </select>
+  );
+}
---
+++ src/validations/FormAdd.validate.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+export const FormAddSchema = z.object({
+  categoria: z
+    .string({ message: "La categoría tiene que ser texto" })
+    .nonempty({ message: "La categoría no puede estar vacía" }),
+  producto: z
+    .string({ message: "El nombre del producto tiene que ser texto" })
+    .nonempty({ message: "El nombre del producto no puede estar vacío" }),
+  marca: z
+    .string()
+    .nonempty({ message: "La marca del producto no puede estar vacía" }),
+  estado: z.string(),
+  stock: z.coerce
+    .number({ invalid_type_error: "El monto debe ser un número" })
+    .positive({ message: "El monto debe ser mayor que 0" }),
+  precioC: z.coerce
+    .number({ invalid_type_error: "El monto debe ser un número" })
+    .positive({ message: "El monto debe ser mayor que 0" }),
+  precioV: z.coerce
+    .number({ invalid_type_error: "El monto debe ser un número" })
+    .positive({ message: "El monto debe ser mayor que 0" }),
+});
+
+/*
+producto: z.string({ message: "El nombre del producto tiene que ser texto" })
+    .refine(value => !/^\d+$/.test(value), {
+      message: "El nombre del producto no puede ser solo números",
+    }),
+*/
