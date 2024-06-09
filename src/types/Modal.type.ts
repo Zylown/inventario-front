a8369da9bdfec8df5f0da2bcadbfceeb4f0818af
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-06-08 22:26:54 -0500

    modal types name restructuring

---
+++ src/types/Modal.type.ts
@@ -0,0 +1,20 @@
+export interface ModalAddData {
+  categoria: string;
+  producto: string;
+  marca: string;
+  estado: string;
+  stock: number;
+  precioC: number;
+  precioV: number;
+}
+
+export interface ModalEditData {
+  id: string;
+  categoria: string;
+  producto: string;
+  marca: string;
+  estado: string;
+  stock: number;
+  precioC: number;
+  precioV: number;
+}
