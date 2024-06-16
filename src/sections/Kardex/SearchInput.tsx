18a81d3846705df3a0689ed08e9f1c1144159b78
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-06-16 15:35:49 -0500

    try add DatePicker for view kardex

--- src/sections/Kardex/SearchInput.tsx
+++ src/sections/Kardex/SearchInput.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function SearchInput() {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
   //2024-06-05 o 2024-12-28
   return (
     <div className="flex flex-grow items-center sm:gap-9 gap-2">
-      <label className="text-white font-semibold text-lg" htmlFor="date">
-        Buscar fecha:{" "}
+      <label className="text-white font-semibold text-lg text-nowrap">
+        Buscar fecha{" "}
       </label>
-      <input
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
         className="w-full p-2 bg-crema rounded-lg outline-none"
-        type="date"
-        onChange={(e) => console.log(e.target.value)}
       />
     </div>
   );
+}
+{
+  /* <input
+        className="w-full p-2 bg-crema rounded-lg outline-none"
+        type="date"
+        onChange={(e) => console.log(e.target.value)}
+      /> */
 }
