e538255132b28082cb4fa93f507fe31afcff5184
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-06-17 00:17:43 -0500

    add spanish to react-datepicker

--- src/sections/Kardex/SearchInput.tsx
+++ src/sections/Kardex/SearchInput.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 import { TbCalendarSearch } from "react-icons/tb";
+import { es } from "date-fns/locale";
 import "react-datepicker/dist/react-datepicker.css";
+
+const locale = es;
+registerLocale("es", locale);
 
 export default function SearchInput() {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
@@ -17,10 +21,11 @@ export default function SearchInput() {
       </label>
       <div className="w-full">
         <DatePicker
+          locale={locale}
           selected={startDate}
           onChange={handleDateChange}
           className="w-full p-2 bg-crema rounded-lg outline-none hover:bg-crema-oscura transition-all"
-          dateFormat={"dd/MM/yyyy"}
+          dateFormat={" d MMMM, yyyy - dd/MM/yyyy"}
           icon={<TbCalendarSearch />}
           showIcon
         />
