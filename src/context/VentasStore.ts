3d91145e52d052202d2849c394403d8106b10eb1
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-06-21 23:36:49 -0500

    add context for Ventas and validations

---
+++ src/context/VentasStore.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+import { VentasProps } from "../types/Kardex.types";
+
+type StoreState = {
+  ventas: VentasProps[];
+  addVenta: (venta: VentasProps) => void;
+};
+
+// aca creamos el store para las ventas que nos permitira agregar una venta a la lista de ventas
+export const useVentasStore = create<StoreState>((set) => ({
+  ventas: [],
+  addVenta: (venta) =>
+    set((state) => ({
+      ventas: [...state.ventas, venta],
+    })),
+}));
--- src/validations/FormProducto.validate.ts
+++ src/validations/FormProducto.validate.ts
@@ -6,5 +6,7 @@ export const FormProductoSchema = z.object({
     .positive({ message: "El monto debe ser mayor que 0" }),
   nombreProducto: z.string().nonempty(),
   precioProducto: z.string().nonempty(),
-  cantidadProducto: z.number().int(),
+  cantidadProducto: z.coerce
+    .number({ invalid_type_error: "El monto debe ser un número" })
+    .positive({ message: "El monto debe ser mayor que 0" }),
 });
