26fabc3a60f5e6777a53342e33a12065d027de0a
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-06-24 18:31:07 -0500

    update props homeLayout

--- src/Layout/HomeLayout.tsx
+++ src/Layout/HomeLayout.tsx
@@ -1,8 +1,8 @@
-export default function HomeLayout({
-  children,
-}: {
+interface HomeLayoutProps {
   children: React.ReactNode;
-}) {
+}
+
+export default function HomeLayout({ children }: HomeLayoutProps) {
   return (
     <main className="bg-[#222222] sm:px-2 md:px-6 px-1 sm:py-2 py-0">
       {children}
