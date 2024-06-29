57149f9481d8926c75c79b3a6ac595216c53ae29
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-06-29 18:51:57 -0500

    update InventarioStoer

--- src/context/InventarioStore.ts
+++ src/context/InventarioStore.ts
@@ -1,37 +1,39 @@
 import { create } from "zustand";
-// import data from "../dataInventario.json";
 import { InventarioProps } from "../types/Inventario.types";
 import { getInventario } from "../api/fetchInventario";
 
-// Definimos el estado de la aplicación y las funciones que se van a utilizar
 type StoreState = {
   products: InventarioProps[];
+  setProducts: (products: InventarioProps[]) => void;
   addProduct: (product: InventarioProps) => void;
   editProduct: (product: InventarioProps) => void;
 };
 
+// Crea la tienda inicialmente con productos vacíos
+export const useInventarioStore = create<StoreState>((set) => ({
+  products: [],
+  setProducts: (products) => set({ products }),
+  addProduct: (product) =>
+    set((state) => ({
+      products: [...state.products, product],
+    })),
+  editProduct: (editProduct) =>
+    set((state) => {
+      const updatedProducts = state.products.map((p) => {
+        if (String(p.id) === String(editProduct.id)) {
+          return editProduct;
+        }
+        return p;
+      });
+      return { products: updatedProducts };
+    }),
+}));
+
+// Inicializa la tienda de manera asíncrona
 const initializeStore = async () => {
   const initialData: InventarioProps[] = await getInventario();
-
-  return create<StoreState>((set) => ({
-    products: initialData,
-    addProduct: (product) =>
-      set((state) => ({
-        products: [...state.products, product],
-      })),
-    editProduct: (editProduct) =>
-      set((state) => {
-        const updatedProducts = state.products.map((p) => {
-          if (String(p.id) === String(editProduct.id)) {
-            return editProduct;
-          }
-          return p;
-        });
-        return {
-          products: updatedProducts,
-        };
-      }),
-  }));
+  const { setProducts } = useInventarioStore.getState();
+  setProducts(initialData);
 };
 
-export const useInventarioStore = await initializeStore();
+initializeStore();
