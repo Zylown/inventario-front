59fca0a0f5934fb74c80a8dbedcf599640d1ba53
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-06-29 21:18:34 -0500

    update

--- src/api/fetchInventario.ts
+++ src/api/fetchInventario.ts
@@ -6,8 +6,9 @@ const API_URL = import.meta.env.VITE_API_URL;
 //GET /inventario
 export const getInventario = async () => {
   try {
-    const response = await axios.get(`${API_URL}/inventario`);
-    // console.log("Data fetched:", response.data)
+    const response = await axios.get(`${API_URL}/inventario`, {
+      withCredentials: true, // Asegura que las credenciales se envíen con la solicitud
+    });
     return response.data;
   } catch (error) {
     console.error(error);
@@ -17,8 +18,9 @@ export const getInventario = async () => {
 //POST /inventario
 export const addInventario = async (data: InventarioProps) => {
   try {
-    const response = await axios.post(`${API_URL}/inventario`, data);
-    // console.log("Data added:", response.data)
+    const response = await axios.post(`${API_URL}/inventario`, data, {
+      withCredentials: true, // Asegura que las credenciales se envíen con la solicitud
+    });
     return response.data;
   } catch (error) {
     console.error(error);
@@ -28,8 +30,9 @@ export const addInventario = async (data: InventarioProps) => {
 //PUT /inventario/:id
 export const editInventario = async (id: number, data: InventarioProps) => {
   try {
-    const response = await axios.put(`${API_URL}/inventario/${id}`, data);
-    // console.log("Data edited:", response.data)
+    const response = await axios.put(`${API_URL}/inventario/${id}`, data, {
+      withCredentials: true, // Asegura que las credenciales se envíen con la solicitud
+    });
     return response.data;
   } catch (error) {
     console.error(error);
